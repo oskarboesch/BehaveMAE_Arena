5cf2498b3f695522f4a81698a199ccb2d94e7d70
oskarboesch <oskar.boesch@epfl.ch>
2026-03-05 11:12:35 +0100

    modifs of settings

--- scripts/arena/test_hBehaveMAE.sh
+++ scripts/arena/test_hBehaveMAE.sh
@@ -2,35 +2,28 @@
 experiment=experiment1
 
 python run_test.py \
-    --path_to_data_dir data/Shot7M2/test/test_dictionary_poses.npy \
-    --dataset shot7m2 \
+    --path_to_data_dir data/Arena_Data/shuffle-3_split-test.npz \
+    --dataset arena \
     --embedsum False \
-    --fast_inference True \
+    --fast_inference False \
     --batch_size 512 \
     --model gen_hiera \
-    --input_size 400 1 72 \
+    --input_size 400 1 54 \
     --stages 2 3 4 \
-    --q_strides "2,1,4;2,1,6" \
+    --q_strides "2,1,1;2,1,1" \
     --mask_unit_attn True False False \
-    --patch_kernel 2 1 3 \
+    --patch_kernel 2 1 54 \
     --init_embed_dim 96 \
     --init_num_heads 2 \
     --out_embed_dims 78 128 256 \
     --distributed \
     --num_frames 400 \
     --pin_mem \
-    --num_workers 0 \
-    --output_dir outputs/shot7m2/${experiment}
+    --num_workers 8 \
+    --output_dir outputs/arena/${experiment}
 
 
 cd hierAS-eval
-
-nr_submissions=$(ls ../outputs/shot7m2/${experiment}/test_submission_* 2>/dev/null | wc -l)
+    
+nr_submissions=$(ls ../outputs/arena/${experiment}/test_submission_* 2>/dev/null | wc -l)
 files=($(seq 0 $((nr_submissions - 1))))
-
-parallel --line-buffer \
-    python evaluator.py \
-        --task Shot7M2 --output-dir results \
-        --labels ../data/Shot7M2/test/benchmark_labels.npy \
-        --submission ../outputs/shot7m2/${experiment}/test_submission_{}.npy \
-    ::: "${files[@]}"
