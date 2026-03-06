5ceef892a6eca6fe9bd78cb964e1403f758ba833
oskarboesch <oskar.boesch@epfl.ch>
2026-03-06 17:35:47 +0100

    change data path + new model script

--- scripts/arena/test_hBehaveMAE_1.sh
+++ scripts/arena/test_hBehaveMAE_1.sh
@@ -1,25 +1,24 @@
 
-experiment=experiment1
+experiment=experiment2
 
 python run_test.py \
-    --path_to_data_dir ../../../scratch/izar/boesch/data/Arena_Data/shuffle-3_split-test.npz \
+    --path_to_data_dir ../../../scratch/boesch/ata/Arena_Data/shuffle-3_split-test.npz \
     --dataset arena \
     --embedsum False \
     --fast_inference False \
     --batch_size 512 \
     --model gen_hiera \
-    --input_size 400 1 54 \
-    --stages 2 3 4 \
-    --q_strides "2,1,1;2,1,1" \
+    --input_size 900 1 54 \
+    --stages 3 4 5 \
+    --q_strides "5,1,1;5,1,1" \
     --mask_unit_attn True False False \
-    --patch_kernel 2 1 54 \
+    --patch_kernel 1 1 54 \
     --init_embed_dim 96 \
     --init_num_heads 2 \
-    --out_embed_dims 78 128 256 \
+    --out_embed_dims 128 192 256 \
     --distributed \
-    --num_frames 400 \
-    --pin_mem \
-    --num_workers 8 \
+    --num_frames 900 \
+    --num_workers 0 \
     --output_dir outputs/arena/${experiment}
 
 
